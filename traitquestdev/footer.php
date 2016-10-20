a5670dc3f928f610879dea6ced454e8d97143411
wilsontanwm <wm_7an888@hotmail.com>
2016-10-20 22:22:51 +0800

    Modified backend for registering company process
    - Added expiry date and max size for free trial license
    - Added address column
    -Modified slightly on the frontend ie color of button, font color &
    size, footer

--- traitquestdev/footer.php
+++ traitquestdev/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer">
   <div class="col-md-12 footer-copyright">
-    <p class="text-center white padding-top-xs">(c) 2016 Traitquest. All Rights Reserved.</p>
+    <p class="text-center fontsize-s white padding-top-xs">&copy; 2016 TraitQuest. All rights reserved.</p>
   </div>
 </footer><!-- footer -->
--- traitquestserver/registerprocess.php
+++ traitquestserver/registerprocess.php
@@ -12,25 +12,25 @@
 		
 		if (!isset($_SESSION['userID'])){// if user is logged out
 			$data['userLoggedIn'] = false;
-			$address='';
 			if (isset($_POST['company']))				            $company	= trim($_POST ['company']);
-			if (isset($_POST['name']))				                $name		= trim($_POST ['name']);
 			if (isset($_POST['email']))			                    $email 		= trim($_POST ['email']);
 			if (isset($_POST['phone']))			                    $phone 		= trim($_POST ['phone']);			
-			if (isset($_POST['address']))		                    $address 		= trim($_POST ['address']);
-			//if (isset($_POST['password']))			                $password 		= $_POST ['password'];
+			if (isset($_POST['address']))		                    $address 	= trim($_POST ['address']);
 																	
-
-			if(!trim($_POST['name'])){//if user's name is filled up
-				$data['name'] = "Enter your name";
+			if(!trim($_POST['company'])){//if user's name is filled up
+				$data['company'] = "Enter your company name";
 				$validated = false;
 			}
 			if (!preg_match("/^[[:alnum:]][a-z0-9_.-]*@[a-z0-9.-]+\.[a-z]{2,4}$/i", trim($_POST['email']))){
-				$data['email'] = "You have entered an invalid email address";
+				$data['email'] = "Enter a valid email address";
 				$validated = false;
 			}
 			if (!trim($_POST['phone'])){
 				$data['phone'] = "Enter your phone number";
+				$validated = false;
+			}
+			if (!trim($_POST['address'])){
+				$data['address'] = "Enter your address";
 				$validated = false;
 			}
 			/*if(strlen($password) >20 || strlen($password)<8){
@@ -49,168 +49,128 @@
 								 WHERE name = :name
 								 LIMIT 1";
 				$companyPDO = $conn->prepare($checkSQL);
-				$companyPDO->bindParam(':name', $company, PDO::PARAM_STR);
-				$companyPDO->execute();
+				$companyPDO->execute(array(
+								   ':name'=> $company
+								   ));
 				
-				if($companyPDO->rowCount() > 0){
-					$companyResult = $companyPDO->fetch(PDO::FETCH_ASSOC);
-					$companyID = $companyResult['id'];
+				if($companyPDO->rowCount() == 0){
 					
-					//check if the email has already been used
-					$checkEmployeeSQL = "SELECT * FROM employee
-											WHERE email = :email
-											AND companyid = :companyid
-											LIMIT 1";
-					$checkEmployeePDO = $conn->prepare($checkEmployeeSQL);
-					$checkEmployeePDO->execute(array(
-											   ':email'=>$email,
-											   ':companyid'=>$companyID
-											   ));
+					$randomPassword = randomPassword();
+					$password = md5('12345678'); // to replace hardcoded string with randomPassword() in release version
+					$maxsize = 5; // max number of a company
+					$registerdate = date ('Y-m-d'); // register date of the company
+					$expirydate = date('Y-m-d', strtotime("+31 days")); // the expiry date of the company license
+					$isadmin = 1;
+					// insert the company and insert admin as user
+					$registerCompanySQL = "INSERT INTO `company` ( name, email, password, phone, address, maxsize, registerdate, expirydate ) 
+													VALUES (:name, :email, :password, :phone, :address, :maxsize, :registerdate, :expirydate);
+							SET @lastcompanyid = LAST_INSERT_ID();
+							INSERT INTO `employee` (companyid, name, email, password, isadmin) 
+												    VALUES (@lastcompanyid, 'Admin', :email, :password, :isadmin);";
+					$registerCompanyPDO = $conn->prepare($registerCompanySQL);
+					$registerCompanyPDO->execute(array(
+												':name'=> $company,
+												':email'=> $email,
+												':password'=> $password,
+												':phone'=> $phone,
+												':address'=> $address,
+												':maxsize'=> $maxsize,
+												':registerdate'=> $registerdate,
+												':expirydate'=> $expirydate,
+												':isadmin'=> $isadmin
+												));						
+						
+					//INPUT CODE HERE TO SEND EMAIL TO USER ON NEW PASSWORD
+					/*$to = $email;
+
+					$subject = 'TraitQuest Registration';
+
+					$headers = "From: noreply@traitquest.com\r\n";
+					$headers .= "MIME-Version: 1.0\r\n";
+					$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+					$urlParam = "cid=".$companyID."&id=".$lastEmployeeID."&code=".$password;
+					$message = '<html>
+					<head>
+					   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+					   <title></title>
+					   <style type="text/css">
+						body, #main h1, #main h2, p {margin: 0; padding: 0;}
+						a{cursor: pointer; color: #EB9532; font-size: 18px;}
+							#main {border: 1px solid #cfcece;}
+							img {display: block;}
+							#top-message p, #bottom-message p {color: #3f4042; font-size: 12px; font-family: Arial, Helvetica, sans-serif; }
+							#main h1 {color: #555 !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; font-size: 24px; margin-bottom: 0!important; padding-bottom: 0; }
+							#main h2 {color: #555 !important; font-family: Arial, Helvetica, sans-serif; font-size: 24px; margin-bottom: 0 !important; padding-bottom: 0; }
+							#main p {color: #555 !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; font-size: 16px;  }
+							h1, h2, h3, h4, h5, h6 {margin: 0 0 0.8em 0;}
+							h3 {font-size: 28px; color: #444444 !important; font-family: Arial, Helvetica, sans-serif; }
+							h4 {font-size: 22px; color: #4A72AF !important; font-family: Arial, Helvetica, sans-serif; }
+							h5 {font-size: 18px; color: #444444 !important; font-family: Arial, Helvetica, sans-serif; }
+							p {font-size: 12px; color: #444444 !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; line-height: 1.5;}
+						</style>
+					</head>
+					<body>
+					<table width="100%" cellpadding="0" cellspacing="0" bgcolor="F9F9F9"><tr><td>
+
+						<table id="top-message" cellpadding="20" cellspacing="0" width="600" align="center">
+							<tr>
+								<td align="center">
+									<a href="http://www.traitquest.com"><img src="http://www.traitquest.com/wp-content/uploads/2016/08/cropped-editedTQ-5.jpg" style="width: 150px;"/></a>
+								</td>
+							</tr>
+						</table><!-- top message -->
+						<table id="main" width="600" align="center" cellpadding="0" cellspacing="15" bgcolor="ffffff">
+							<tr>
+								<td>
+									<h1>TraitQuest Registration</h1>
+								</td>
+							</tr><!-- header -->
+							<tr>
+								<td>
+									<p>Hi '.$name.',</p>
+									<p>Thank you for registering to TraitQuest.</p>
+									<p>Your account details are as follow:<br/>
+									   Email: '.$email.'<br/>
+									   Password: '.$randomPassword.'
+									</p>
+									<p>To complete your registration, please verify your account by clicking on the following link:</p>
+									<table id="email-link" cellpadding="5" cellspacing="0" width="400" align="center">
+										<tr>
+											<td align="center">
+												<a href="http://www.traitquest.com/verify?'.$urlParam.'">Click here to login</a>
+											</td>
+										</tr>
+									</table>
+									<p>Best wishes, <br/>The TraitQuest Team</p>
+								</td>
+							</tr>
+
+						</table>
+						<table id="footer-message" cellpadding="5" cellspacing="0" width="600" align="center">
+							<tr>
+								<td align="center">
+									<span>&copy; 2016 TraitQuest. All rights reserved.</span>
+								</td>
+							</tr>
+						</table>
+					</td></tr></table>
+					</body>
+					</html>';
+					mail($to, $subject, $message, $headers);
+
+					$emailTo = 'activation@traitquest.com';
+					$alternativeBody = "Name: $name \n\nEmail: $email \n\nActivation Link: http://traitquest.com/verify?".$urlParam;
+					$alternativeHeaders = $name. 'Account';
+
+					mail($emailTo, $alternativeHeaders, $alternativeBody, $alternativeHeaders);*/
+					$data['registered'] = true;
 					
-					if($checkEmployeePDO->rowCount() == 0){
-						//$emailActivationCode = md5($email.time()); //for activation of the email use
-						$randomPassword = randomPassword();
-						$password = md5($randomPassword);// to replace hardcoded string with randomPassword() in release version
-						
-						// insert the user into the event
-						$sql = "INSERT INTO `employee` (companyid, name, email, password, phone, address) 
-														VALUES (:companyid, :name, :email, :password, :phone, :address);";
-						$q = $conn->prepare($sql);
-						$q->execute(array(
-									':companyid'=> $companyID,
-									':name'=> $name,
-									':email'=> $email,
-									':password'=> $password,
-									':phone'=> $phone,
-									':address'=> $address
-									));
-						$lastEmployeeID = $conn->lastInsertId();
-						
-						// Add KPI 'Take Photo' to the user
-						$selectPhotoTemplateSQL = "SELECT * FROM phototemplate
-													ORDER BY rand() 
-													LIMIT 1";
-						$selectPhotoTemplatePDO = $conn->prepare($selectPhotoTemplateSQL);
-						$selectPhotoTemplatePDO->execute();
-						if( $selectPhotoTemplatePDO->rowCount() != 0 ){
-							$photoTemplateResult = $selectPhotoTemplatePDO->fetch(PDO::FETCH_ASSOC);
-							$kpiType = 'photo';
-							$photoTemplateID = $photoTemplateResult['id'];
-							$addKPIPhotoSQL = "INSERT INTO `kpi` (userid, companyid, type, phototemplateid)
-																VALUES (:userid, :companyid, :type, :phototemplateid);";
-							$addKPIPhotoPDO = $conn->prepare($addKPIPhotoSQL);
-							$addKPIPhotoPDO->execute(array(
-													':userid'=> $lastEmployeeID,
-													':companyid'=> $companyID,
-													':type'=> $kpiType,
-													':phototemplateid'=> $photoTemplateID));	
-						}
-						// Add KPI 'Quiz' to the user
-						$kpiType = 'quiz';
-						$addKPIQuizSQL = "INSERT INTO `kpi` (userid, companyid, type)
-																VALUES (:userid, :companyid, :type);";
-						$addKPIQuizPDO = $conn->prepare($addKPIQuizSQL);
-						$addKPIQuizPDO->execute(array(
-												':userid'=>$lastEmployeeID,
-												':companyid'=> $companyID,
-												':type'=>$kpiType));	
-
-						//INPUT CODE HERE TO SEND EMAIL TO USER ON NEW PASSWORD
-						/*$to = $email;
-
-						$subject = 'TraitQuest Registration';
-
-						$headers = "From: noreply@traitquest.com\r\n";
-						$headers .= "MIME-Version: 1.0\r\n";
-						$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-						$urlParam = "cid=".$companyID."&id=".$lastEmployeeID."&code=".$password;
-						$message = '<html>
-						<head>
-						   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-						   <title></title>
-						   <style type="text/css">
-							body, #main h1, #main h2, p {margin: 0; padding: 0;}
-							a{cursor: pointer; color: #EB9532; font-size: 18px;}
-								#main {border: 1px solid #cfcece;}
-								img {display: block;}
-								#top-message p, #bottom-message p {color: #3f4042; font-size: 12px; font-family: Arial, Helvetica, sans-serif; }
-								#main h1 {color: #555 !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; font-size: 24px; margin-bottom: 0!important; padding-bottom: 0; }
-								#main h2 {color: #555 !important; font-family: Arial, Helvetica, sans-serif; font-size: 24px; margin-bottom: 0 !important; padding-bottom: 0; }
-								#main p {color: #555 !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; font-size: 16px;  }
-								h1, h2, h3, h4, h5, h6 {margin: 0 0 0.8em 0;}
-								h3 {font-size: 28px; color: #444444 !important; font-family: Arial, Helvetica, sans-serif; }
-								h4 {font-size: 22px; color: #4A72AF !important; font-family: Arial, Helvetica, sans-serif; }
-								h5 {font-size: 18px; color: #444444 !important; font-family: Arial, Helvetica, sans-serif; }
-								p {font-size: 12px; color: #444444 !important; font-family: "Lucida Grande", "Lucida Sans", "Lucida Sans Unicode", sans-serif; line-height: 1.5;}
-							</style>
-						</head>
-						<body>
-						<table width="100%" cellpadding="0" cellspacing="0" bgcolor="F9F9F9"><tr><td>
-
-							<table id="top-message" cellpadding="20" cellspacing="0" width="600" align="center">
-								<tr>
-									<td align="center">
-										<a href="http://www.traitquest.com"><img src="http://www.traitquest.com/wp-content/uploads/2016/08/cropped-editedTQ-5.jpg" style="width: 150px;"/></a>
-									</td>
-								</tr>
-							</table><!-- top message -->
-							<table id="main" width="600" align="center" cellpadding="0" cellspacing="15" bgcolor="ffffff">
-								<tr>
-									<td>
-										<h1>TraitQuest Registration</h1>
-									</td>
-								</tr><!-- header -->
-								<tr>
-									<td>
-										<p>Hi '.$name.',</p>
-										<p>Thank you for registering to TraitQuest.</p>
-										<p>Your account details are as follow:<br/>
-										   Email: '.$email.'<br/>
-										   Password: '.$randomPassword.'
-										</p>
-										<p>To complete your registration, please verify your account by clicking on the following link:</p>
-										<table id="email-link" cellpadding="5" cellspacing="0" width="400" align="center">
-											<tr>
-												<td align="center">
-													<a href="http://www.traitquest.com/verify?'.$urlParam.'">Click here to login</a>
-												</td>
-											</tr>
-										</table>
-										<p>Best wishes, <br/>The TraitQuest Team</p>
-									</td>
-								</tr>
-
-							</table>
-							<table id="footer-message" cellpadding="5" cellspacing="0" width="600" align="center">
-								<tr>
-									<td align="center">
-										<span>&copy; 2016 TraitQuest. All rights reserved.</span>
-									</td>
-								</tr>
-							</table>
-						</td></tr></table>
-						</body>
-						</html>';
-						mail($to, $subject, $message, $headers);
-
-						$emailTo = 'activation@traitquest.com';
-						$alternativeBody = "Name: $name \n\nEmail: $email \n\nActivation Link: http://traitquest.com/verify?".$urlParam;
-						$alternativeHeaders = $name. 'Account';
-
-						mail($emailTo, $alternativeHeaders, $alternativeBody, $alternativeHeaders);*/
-						$data['registered'] = true;
-						$urlParam = "cid=".$companyID."&id=".$lastEmployeeID."&code=".$password;
-						$data['redirect'] = "verify?".$urlParam;
-						//$data['message'] = "Your password will be sent to your email address shortly.";				
-					}
-					else{
-						$data['registered'] = false;
-						$data['error'] = "This email has already been registered";
-					}				
+					$data['message'] = "Your account has been created. Your password will be sent to your email address shortly.";				
+								
 				}
 				else{
 					$data['registered'] = false;
-					$data['error'] = "The event has ended. For more info, please contact us at <a href='mailto:support@traitquest.com'>support@traitquest.com</a>";
+					$data['error'] = "Your company has already been registered";
 				}
 			}
 			else{

f473f031f633567a01939efa8c194dc4a9b11b9c
wilsontanwm <wm_7an888@hotmail.com>
2016-12-13 15:30:39 +0800

    Compile employee (admin) page
    - Added the employee (admin) backend page
    - Restructured the folder for employee (admin)

--- traitquestdev/js/admin/editemployee.js
+++ traitquestdev/js/admin/editemployee.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var working = false;
 	var employeeID = getUrlParameter('id');
 	if( employeeID === undefined || employeeID == '' ){
-		window.location.href = "employeemanagement.php";
+		window.location.href = "employee.php";
 	}
 	populateForm();
 	
@@ -33,7 +33,7 @@ $(document).ready(function(){
 		//process the form
 		$.ajax({
 			type		:'POST', 	//define the type of HTTP verb we want to use
-			url			:'data/editemployeeprocess.php',		//the url where we want to POST
+			url			:'../data/admin/employee/editemployeeprocess.php',		//the url where we want to POST
 			data		: formData,		//our data object
 			dataType	:'json',		//what type of data do we expect back from the server
 			encode		:true
@@ -46,30 +46,39 @@ $(document).ready(function(){
 			if(data['adminloggedIn']){				
 				//check if it has errors
                 if(!data['editSuccess']){
-                    $('#editEmployeeResponse').append('<div class="editError text-center red fontsize-xs">' + data['error'] + '</div>');
+                    $('#editEmployeeResponse').append('<div class="editError text-center red">' + data['error'] + '</div>');
                 }
                 else{
 					// redirect after editing
-					//window.location.href = "profile.php";
-					window.location.replace("employeemanagement.php"); // to go back to previous page and refresh
+					window.history.back();
 				}
 			}
 			else{
 				// redirect to home page when user is not logged in as admin
-                window.location.href = "index.php";
+                window.location.href = "../index.php";
 			}
 		})
 		//using the fail promise callback
 		.fail(function(data){
-            //window.location.href = "500.php";
+            window.location.href = "../500.php";
 		});
 		
 	});
 	
+	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			TO CANCEL EDIT
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+	this.getElementById('editCompanyCancel').addEventListener('click',function(){
+		window.history.back();
+	}, false);
+	
+	/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+			TO POPULATE THE FORM
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 	function populateForm(){
 		$.ajax({
 				type		:'POST', 	//define the type of HTTP verb we want to use
-				url			:'data/getemployeedataprocess.php',		//the url where we want to POST
+				url			:'../data/admin/employee/getemployeedataprocess.php',		//the url where we want to POST
 				data		: {'employeeID': employeeID},		//our data object
 				dataType	:'json',		//what type of data do we expect back from the server
 				encode		:true
@@ -78,16 +87,19 @@ $(document).ready(function(){
 		//using the done promise callback
 		.done(function(data){
 			if(data['return']){
+				var imageHTML = '<img class="imageSize100px center padding-xs" src="'+data['employee']['imagelink']+'">';
+				$("#columnEmployeePic").append(imageHTML);
 				$("#employeeName").val( data['employee']['name'] ); 
 				$("#employeeCode").val( data['employee']['code'] );
 			}
 			else{
-				window.location.href = "employeemanagement.php";
+				// go back to previous page
+				window.history.back();
 			}
 		})
 		//using the fail promise callback
 		.fail(function(data){
-			window.location.href = "500.php";
+			window.location.href = "../500.php";
 		});
 	}
 

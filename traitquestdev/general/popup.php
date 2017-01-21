228c7ad01fc8f0910579c08f8a9df0dd92da346a
wilsontanwm <wm_7an888@hotmail.com>
2017-01-21 20:54:21 +0800

    Success, warning and confirmation popup
    - Added success, warning and confirmation popup dialog

---
+++ traitquestdev/general/popup.php
@@ -0,0 +1,52 @@
+<!-- SUCCESS POPUP DIALOG -->
+<div id="successModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="successModal">
+	<div class="modal-dialog modal-sm" role="document">
+		<div class="modal-content padding-l">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<img class="center imageSize100px" src="../../images/popup/success.png" />
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<p id="successMessage" class="text-center"></p>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<input type="button" name="submit" id="successYes" class="center buttonForm button margin-top-xs" value="Yes" />
+				<input type="button" name="cancel" id="successCancel" class="center buttonForm buttonCancel margin-top-xs" value="Close" />
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- CONFIRMATION POPUP DIALOG -->
+<div id="confirmationModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="confirmationModal">
+	<div class="modal-dialog modal-sm" role="document">
+		<div class="modal-content padding-l">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<img class="center imageSize100px" src="../../images/popup/confirmation.png" />
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<p id="confirmationMessage" class="text-center"></p>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<input type="button" name="submit" id="confirmationYes" class="center buttonForm button margin-top-xs" value="Yes" />
+				<input type="button" name="cancel" id="confirmationCancel" class="center buttonForm buttonCancel margin-top-xs" value="Cancel" />
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- WARNING POPUP DIALOG -->
+<div id="errorModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="errorModal">
+	<div class="modal-dialog modal-sm" role="document">
+		<div class="modal-content padding-l">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<img class="center imageSize100px" src="../../images/popup/error.png" />
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<p id="errorMessage" class="text-center"></p>
+			</div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<input type="button" name="cancel" id="errorCancel" class="center buttonForm buttonCancel margin-top-xs" value="Close" />
+			</div>
+		</div>
+	</div>
+</div>
